2b560b0df212127781b463620fcde46b3be03115
jasimkasba <saldanodi88@gmail.com>
2020-08-24 05:52:25 +0600

    need time

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+
+    // const total = cart.reduce((total, prd) => total + prd.price, 0);
+
+    let total = 0;
+    for(let i = 0; i < cart.length; i++){
+        const product = cart[i];
+        total = total + product.price;
+    }
+    return (
+        <div>
+            <h4>Order summary</h4>
+            <p>Items Ordered:{cart.length} </p>
+            <p>Total Price : {total}</p>
+
+        </div>
+    );
+};
+
+export default Cart;
