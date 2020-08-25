412213c837e9658b6c615fbe228437964e3627c1
jasimkasba <saldanodi88@gmail.com>
2020-08-25 08:14:05 +0600

    shop page done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,11 +10,35 @@ const Cart = (props) => {
         const product = cart[i];
         total = total + product.price;
     }
+
+    let shipping = 0;
+
+    if(total > 35){
+        shipping = 0;
+    }
+    else if(total > 15){
+        shipping = 4.99
+    }
+    else if(total > 0){
+        shipping = 12.99
+    }
+     
+    const tax = total/10;
+    const grandTotal = (total + shipping +Number(tax)).toFixed(2);
+
+    const formatNumber = num =>{
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
     return (
         <div>
             <h4>Order summary</h4>
             <p>Items Ordered:{cart.length} </p>
-            <p>Total Price : {total}</p>
+            <p>Product price :{formatNumber(total)}</p>
+            <p><small>shipping cost:{shipping} </small></p>
+            <p><small>Tax + vat: {formatNumber(tax)}</small></p>
+            <p>Total Price : {grandTotal}</p>
 
         </div>
     );
